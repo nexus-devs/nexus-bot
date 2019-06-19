11cfa2a1a0dac958665596dd4336f0a2c2cf3f40
Nakroma <tarackobarack@gmail.com>
2019-06-19 14:39:46 +0200

    added more detailed price-check description

--- commands/prices/price-check.js
+++ commands/prices/price-check.js
@@ -4,16 +4,23 @@ class PriceCheck extends Command {
   constructor (client) {
     super(client, {
       name: 'price-check',
-      aliases: ['pc', 'pricecheck', 'pcheck'],
+      aliases: ['pc', 'pcheck'],
       group: 'prices',
       memberName: 'price-check',
-      description: 'Checks an item price.'
+      description: 'Checks an item price.',
+      examples: ['price-check Frost Prime'],
+      args: [{
+        key: 'item-name',
+        label: 'item name',
+        prompt: 'What item would you like to price check?',
+        type: 'string'
+      }]
     })
   }
 
   async run (msg, args) {
     let res
-    try { res = await this.api.get(`/warframe/v1/items/${args}/prices`) }
+    try { res = await this.api.get(`/warframe/v1/items/${args['item-name']}/prices`) }
     catch (err) {
       return msg.reply(`${err.error} ${err.reason}`)
     }
@@ -22,7 +29,7 @@ class PriceCheck extends Command {
     if (!set) return msg.reply('Could not fetch a general price')
 
     const setPrice = Math.round((set.prices.buying.current.median + set.prices.selling.current.median) / 2)
-    return msg.reply(`Price for ${res.name}: ${setPrice}p`)
+    return msg.reply(`Price for ${res.name}: \`${setPrice}p\``)
   }
 }
 
