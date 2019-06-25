c25ef60c6dd3bb7d6d0355107f8cb3d5fa1c5e1a
Nakroma <tarackobarack@gmail.com>
2019-06-25 18:32:31 +0200

    extended price check to all components

--- commands/prices/price-check.js
+++ commands/prices/price-check.js
@@ -32,31 +32,34 @@ class PriceCheck extends Command {
     catch (err) {
       return msg.reply(`${err.error} ${err.reason}`)
     }
-
-    const set = res.components.find((comp) => { return comp.name === 'Set' })
-    if (!set) return msg.reply('Could not fetch a general price')
-
-    const currentPrice = Math.round((set.prices.buying.current.median + set.prices.selling.current.median) / 2)
-    const previousPrice = Math.round((set.prices.buying.previous.median + set.prices.selling.previous.median) / 2)
-    const pricePercentage = Math.round((currentPrice - previousPrice) / previousPrice * 100)
-
-    const orderTotal = set.prices.buying.current.orders + set.prices.selling.current.orders
-    const buyerPercentage = Math.round((set.prices.buying.current.orders / orderTotal) * 100)
-    const sellerPercentage = Math.round((set.prices.selling.current.orders / orderTotal) * 100)
-
-    // Get the highest buy order and the lowest sell order
     orders = orders.filter(order => order.price !== null)
-    const bestBuyOrder = orders.filter(order => order.offer === 'Buying' && order.component === 'Set')
-      .reduce((prev, curr) => prev.price > curr.price ? prev : curr)
-    const bestSellOrder = orders.filter(order => order.offer === 'Selling' && order.component === 'Set')
-      .reduce((prev, curr) => prev.price < curr.price ? prev : curr)
 
-    let percentageEmoji = pricePercentage < 0 ? '<:arrowdown:593103530613538816>' : '<:arrowup:593102737236033536>'
-    let text = `**${res.name}: ${currentPrice}p**     ${percentageEmoji}${pricePercentage}%\n`
-    text += '─────────────────────────\n'
-    text += `Buyers: ${set.prices.buying.current.orders} (${buyerPercentage}%)     Sellers: ${set.prices.selling.current.orders} (${sellerPercentage}%)\n`
-    text += `Best buy order: ${bestBuyOrder.price}p from \`${bestBuyOrder.user}\`\n`
-    text += `Best sell order: ${bestSellOrder.price}p from \`${bestSellOrder.user}\`\n`
+    // const set = res.components.find((comp) => { return comp.name === 'Set' })
+    // if (!set) return msg.reply('Could not fetch a general price')
+
+    let text = ''
+    for (const comp of res.components) {
+      const currentPrice = Math.round((comp.prices.buying.current.median + comp.prices.selling.current.median) / 2)
+      const previousPrice = Math.round((comp.prices.buying.previous.median + comp.prices.selling.previous.median) / 2)
+      const pricePercentage = Math.round((currentPrice - previousPrice) / previousPrice * 100)
+
+      const orderTotal = comp.prices.buying.current.orders + comp.prices.selling.current.orders
+      const buyerPercentage = Math.round((comp.prices.buying.current.orders / orderTotal) * 100)
+      const sellerPercentage = Math.round((comp.prices.selling.current.orders / orderTotal) * 100)
+
+      // Get the highest buy order and the lowest sell order
+      const bestBuyOrder = orders.filter(order => order.offer === 'Buying' && order.component === comp.name)
+        .reduce((prev, curr) => prev.price > curr.price ? prev : curr)
+      const bestSellOrder = orders.filter(order => order.offer === 'Selling' && order.component === comp.name)
+        .reduce((prev, curr) => prev.price < curr.price ? prev : curr)
+
+      let percentageEmoji = pricePercentage < 0 ? '<:arrowdown:593103530613538816>' : '<:arrowup:593102737236033536>'
+      text += `**${res.name} ${comp.name}: ${currentPrice}p**     ${percentageEmoji}${pricePercentage}%\n`
+      // text += '─────────────────────────\n'
+      text += `Buyers: ${comp.prices.buying.current.orders} (${buyerPercentage}%)     Sellers: ${comp.prices.selling.current.orders} (${sellerPercentage}%)\n`
+      text += `Best buy order: ${bestBuyOrder.price}p from \`${bestBuyOrder.user}\`\n`
+      text += `Best sell order: ${bestSellOrder.price}p from \`${bestSellOrder.user}\`\n\n`
+    }
     return msg.reply(text)
   }
 }
