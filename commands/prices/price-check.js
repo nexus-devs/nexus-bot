a69a6d6cfa4ba0f476ddb6e1e4530a79fae1ac90
Nakroma <tarackobarack@gmail.com>
2019-06-26 17:43:47 +0200

    changed formatting

--- commands/prices/price-check.js
+++ commands/prices/price-check.js
@@ -20,26 +20,23 @@ class PriceCheck extends Command {
   }
 
   async run (msg, args) {
-    // Get item price data
-    let res
-    try { res = await this.api.get(`/warframe/v1/items/${args['item-name']}/prices`) }
-    catch (err) {
+    // Get item price data, orders for that item and corresponding meta data
+    let res, orders, meta
+    try {
+      res = await this.api.get(`/warframe/v1/items/${args['item-name']}/prices`)
+      orders = await this.api.get(`/warframe/v1/orders?item=${args['item-name']}`)
+      meta = await this.api.get(`/warframe/v1/items/${args['item-name']}`)
+    } catch (err) {
       return msg.reply(`${err.error} ${err.reason}`)
     }
 
-    // Get all orders corresponding to that item
-    let orders
-    try { orders = await this.api.get(`/warframe/v1/orders?item=${args['item-name']}`) }
-    catch (err) {
-      return msg.reply(`${err.error} ${err.reason}`)
-    }
     orders = orders.filter(order => order.price !== null)
 
     const embed = new RichEmbed()
       .setColor('#11acb2')
       .setTitle(res.name)
+      .setURL('https://nexushub.co' + meta.webUrl)
       .setAuthor('NexusHub', 'https://nexushub.co/img/brand/nexushub-logo-bw.png', 'https://nexushub.co/')
-      .setTimestamp()
 
     for (const comp of res.components) {
       const currentPrice = Math.round((comp.prices.buying.current.median + comp.prices.selling.current.median) / 2)
@@ -50,17 +47,24 @@ class PriceCheck extends Command {
       const buyerPercentage = Math.round((comp.prices.buying.current.orders / orderTotal) * 100)
       const sellerPercentage = Math.round((comp.prices.selling.current.orders / orderTotal) * 100)
 
-      // Get the highest buy order and the lowest sell order
-      const bestBuyOrder = orders.filter(order => order.offer === 'Buying' && order.component === comp.name)
-        .reduce((prev, curr) => prev.price > curr.price ? prev : curr)
-      const bestSellOrder = orders.filter(order => order.offer === 'Selling' && order.component === comp.name)
-        .reduce((prev, curr) => prev.price < curr.price ? prev : curr)
+      // Get the highest buy order
+      let bestBuyOrder = orders.filter(order => order.offer === 'Buying' && order.component === comp.name)
+      if (bestBuyOrder.length > 0) bestBuyOrder = bestBuyOrder.reduce((prev, curr) => prev.price > curr.price ? prev : curr)
+      else bestBuyOrder = null
+
+      // Get the lowest sell order
+      let bestSellOrder = orders.filter(order => order.offer === 'Selling' && order.component === comp.name)
+      if (bestSellOrder.length > 0) bestSellOrder = bestSellOrder.reduce((prev, curr) => prev.price < curr.price ? prev : curr)
+      else bestSellOrder = null
 
       let percentageEmoji = pricePercentage < 0 ? '<:arrowdown:593103530613538816>' : '<:arrowup:593102737236033536>'
       let text = `${currentPrice}p     ${percentageEmoji}${pricePercentage}%\n`
       text += `Buyers: ${comp.prices.buying.current.orders} (${buyerPercentage}%)     Sellers: ${comp.prices.selling.current.orders} (${sellerPercentage}%)\n`
-      text += `Best buy order: ${bestBuyOrder.price}p from \`${bestBuyOrder.user}\`\n`
-      text += `Best sell order: ${bestSellOrder.price}p from \`${bestSellOrder.user}\``
+
+      if (!bestBuyOrder) text += 'No best buy order currently available.\n'
+      else text += `Best buy order: ${bestBuyOrder.price}p from \`${bestBuyOrder.user}\`\n`
+      if (!bestSellOrder) text += 'No best sell order currently available.\n'
+      else text += `Best sell order: ${bestSellOrder.price}p from \`${bestSellOrder.user}\``
 
       embed.addField(`${comp.name}`, text)
     }
