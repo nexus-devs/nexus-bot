81573fa5a07cd1871282baa546d393bf92025613
Nakroma <tarackobarack@gmail.com>
2019-08-06 19:50:43 +0200

    added debug stuff

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Commando = require('discord.js-commando')
 const path = require('path')
 const config = require('./config.js')
+const fs = require('fs')
 
 const client = new Commando.Client({
   commandPrefix: 'nex',
@@ -16,6 +17,11 @@ client.registry
 client.on('ready', () => {
   console.log('Bot ready!')
   client.user.setActivity('NexusHub.co')
+})
+client.on('error', (e) => {
+  fs.writeFile('/tmp/botlog.txt', e.toString(), 'utf8', (err) => {
+    if (err) throw err
+  })
 })
 
 client.login(config.discordToken)
